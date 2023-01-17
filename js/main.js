f335b6b57d9d8c25b4774bfb67e9a80bb403161d
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2023-01-17 14:34:20 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,61 @@
+$('.marquee-with-options').marquee({
+    duration: 5000,
+    gap: 0,
+    delayBeforeStart: 0,
+    direction: 'left',
+    duplicated: true,
+  });
+
+  $('.marquee-with-optionsSider').marquee({
+    duration: 50000,
+    gap: 0,
+    delayBeforeStart: 0,
+    direction: 'left',
+    duplicated: true,
+  });
+
+
+var swiper = new Swiper(".swiperHome", {
+    loop: true,
+    autoplay: {
+        delay: 8000,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+const tituloSlider = document.querySelector("section.sliderHome .container .part1 .info h1")
+const tituloChinoSlider = document.querySelector("section.sliderHome .container .part1 h2")
+const descripcionSlider = document.querySelector("section.sliderHome .container .part1 .info p")
+
+swiper.on('slideChange', function () {
+    var TituloDiapo = document.querySelector(".swiperHome .swiper-slide-next .info .titulo").textContent
+    var tituloChinoDiapo = document.querySelector(".swiperHome .swiper-slide-next .info .tituloChino").textContent
+    var descripcionDiapo = document.querySelector(".swiperHome .swiper-slide-next .info .descripcion").textContent
+
+    tituloSlider.innerHTML = TituloDiapo
+    tituloChinoSlider.innerHTML = tituloChinoDiapo
+    descripcionSlider.innerHTML = descripcionDiapo
+});
+  
+
+var swiper2 = new Swiper(".swiperCategoriasHome", {
+    slidesPerView: 4,
+    spaceBetween: 9,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+  
+  
+  
+  
+
+
+
